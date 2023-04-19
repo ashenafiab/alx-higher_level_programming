f553c71a07141341d630cbcc3f2a596dbc19e90d
Ashenafi Abebaw <ashenafi.abebaw@dventus.com>
2023-04-19 17:29:29 +0300

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
+
 def multiple_returns(sentence):
-    if len(sentence) is 0:
+    """
+    A function that returns a tuple with 
+    the length of a string and its first character.
+    """
+    if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
